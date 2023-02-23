eb42dc6ddb54580fd9e3b4180234267acad36717
Lichitek <71435373+Lichitek@users.noreply.github.com>
2023-02-23 12:50:55 +0300

    preLastMembers

--- Assets/Scripts/turnManager.cs
+++ Assets/Scripts/turnManager.cs
@@ -9,7 +9,24 @@ public class turnManager : MonoBehaviour
     public Text turnText;
     public Text scoreText;
     public int idPlayering;
-    
+    public int numPlayers;
+
+    public List<Sprite> numImage = new List<Sprite>();
+    public Image currentPlayersImage;
+
+    public bool start;
+
+
+
+    void Start()
+    {
+        numPlayers = 2;
+    }
+
+    private void FixedUpdate()
+    {
+        currentPlayersImage.sprite = numImage[numPlayers - 2];
+    }
 
     public void RunTurn(int numberBox)
     {
@@ -22,23 +39,56 @@ public class turnManager : MonoBehaviour
                 break;
 
             case 1:
-                idPlayering = 0;
+                idPlayering = 2;
+                if (numPlayers < (idPlayering + 1))
+                    idPlayering = 0;
                 turnText.text = "Игрок " + (idPlayering + 1).ToString();
                 scoreText.text = players[idPlayering].score.ToString();
                 break;
 
-            /*case 2:
+            case 2:
                 idPlayering = 3;
+                if (numPlayers < (idPlayering + 1))
+                    idPlayering = 0;
                 turnText.text = "Игрок " + (idPlayering + 1).ToString();
                 scoreText.text = players[idPlayering].score.ToString();
                 break;
 
             case 3:
                 idPlayering = 0;
+                
                 turnText.text = "Игрок " + (idPlayering + 1).ToString();
                 scoreText.text = players[idPlayering].score.ToString();
-                break;*/
+                break;
         }
 
+    }
+
+    public void AddUser()
+    {
+        if (numPlayers < 4)
+            numPlayers += 1;
+    }
+
+    public void RemoveUser()
+    {
+        if(numPlayers > 2)
+            numPlayers -= 1;
+    }
+
+    public void startPlay()
+    {
+        start = true;
+        for(int i = 0; i < players.Count; i++)
+        {
+            if (numPlayers > i)
+                continue;
+            else
+            {
+                players.RemoveAt(i);
+                i--;
+            }
+
+        }
     }
 }
